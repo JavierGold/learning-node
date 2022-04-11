2576f3402a112a5ad745f14894995c8883246daa
JavierGold <567498jav@gmail.com>
2022-04-11 12:26:34 -0500

    actualización callback

--- async/index.js
+++ async/index.js
@@ -5,14 +5,17 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`)
 
     superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if(err)return console.log(err.message)
-        console.log(res.body.message);
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .then(res => {
 
-        fs.writeFile(`dog-img.txt`,res.body.message,err =>{
-            if(err)return console.log(err.message)
-            console.log('Random dog img saved to file...');
+            console.log(res.body.message);
+
+            fs.writeFile(`dog-img.txt`, res.body.message, err => {
+                if (err) return console.log(err.message)
+                console.log('Random dog img saved to file...');
+            });
         })
-    });
+        .catch(err => {
+            console.log(err.message)
+        })
 });
