81868ced5e8e837fea8ac78cda3fbb75be64053e
JavierGold <567498jav@gmail.com>
2022-04-11 12:37:29 -0500

    building promise

--- async/index.js
+++ async/index.js
@@ -1,7 +1,18 @@
+const { rejects } = require('assert');
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent')
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePro = file => {
+    return new Promise((resolve,reject)=>{
+        fs.readFile(file,(err,data)=>{
+            if(err) reject('I could not find that file...')
+            resolve(data);
+        })
+    })
+}
+
+readFilePro(`${__dirname}/dog.txt` ).then(data=>{
     console.log(`Breed: ${data}`)
 
     superagent
@@ -18,4 +29,5 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
         .catch(err => {
             console.log(err.message)
         })
-});
+})
+
