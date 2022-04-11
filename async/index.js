8f9d1e23dba58b81a57e25e6671537234ae2865e
JavierGold <567498jav@gmail.com>
2022-04-11 16:26:24 -0500

    async await

--- async/index.js
+++ async/index.js
@@ -12,22 +12,53 @@ const readFilePro = file => {
     })
 }
 
-readFilePro(`${__dirname}/dog.txt` ).then(data=>{
-    console.log(`Breed: ${data}`)
+const writeFilePro=(file,data)=>{
+    return new Promise((resolve,reject)=>{
+        fs.writeFile(file,data,err=>{
+            if(err)reject('Could no write the file...');
+            resolve('sucess');
+        })
+    })
+}
 
-    superagent
+
+const getDogPic = async () =>{
+
+    try {
+        
+   const data = await readFilePro(`${__dirname}/dog.txt` )
+   console.log(`Breed: ${data}`)
+
+   const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+   console.log(res.body.message);
+
+   await writeFilePro(`dog-img.txt`, res.body.message)
+   console.log('Random dog img saved to file...');
+    }
+    catch (err){
+        console.log(err.message)
+    }
+}
+
+getDogPic();
+
+/* 
+readFilePro(`${__dirname}/dog.txt` )
+    .then(data=>{
+    console.log(`Breed: ${data}`)
+    return superagent
         .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
+    })
+    .then(res => {
 
             console.log(res.body.message);
-
-            fs.writeFile(`dog-img.txt`, res.body.message, err => {
-                if (err) return console.log(err.message)
-                console.log('Random dog img saved to file...');
-            });
+            return writeFilePro(`dog-img.txt`, res.body.message)
+        })
+        .then(()=>{
+            console.log('Random dog img saved to file...');
         })
         .catch(err => {
             console.log(err.message)
-        })
-})
+        }) */
+
 
