b83d006526906a123d36472a5984eb8ba5ec1c3a
JavierGold <567498jav@gmail.com>
2022-04-12 12:19:56 -0500

    add morgan

--- natours/app.js
+++ natours/app.js
@@ -1,14 +1,31 @@
 const fs = require('fs')
 const express = require('express');
+const morgan =require('morgan')
 const app = express();
+
+
+
 
 app.use(express.json());
 
+app.use(morgan('dev'));
+
+app.use((req,res,next)=>{
+    console.log('Hola desde el middleware...')
+    next()
+})
+
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toISOString()
+    next()
+})
 const tours = JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTours = (req,res)=>{
+    console.log(req.requestTime)
     res.status(200).json({
         status: 'success',
+        request: req.requestTime,
         results: tours.length,
         data:{
             tours: tours
