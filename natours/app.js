099df1e084e0ec14035df567a3c6ec76f24c4da4
JavierGold <567498jav@gmail.com>
2022-04-12 10:04:29 -0500

    patch

--- natours/app.js
+++ natours/app.js
@@ -25,21 +25,16 @@ app.get('/api/v1/tours',(req,res)=>{
 })
 
 app.get('/api/v1/tours/:id',(req,res)=>{
-
     const id=req.params.id * 1 //al multiplicar hace de un string un int 
-
-    if(id >tours.length){
+    if(id >tours.length || !tour){
         return res.status(404).json({
             status: 'fail',
             message: 'ID inválido'
         })
     }
-
     const tour = tours.find(el=>el.id===id)
-
     res.status(200).json({
         status: 'success',
-        
         data:{
             tour
         }
@@ -48,12 +43,9 @@ app.get('/api/v1/tours/:id',(req,res)=>{
 
 app.post('/api/v1/tours',(req,res)=>{
    /*  console.log(req.body); */
-
    const newId = tours[tours.length-1].id +1;
    const newTour = Object.assign({id: newId }, req.body)
-
    tours.push(newTour);
-
    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
     res.status(201).json({
         status: 'success',
@@ -62,6 +54,21 @@ app.post('/api/v1/tours',(req,res)=>{
         }
     })
    })
+})
+
+app.patch('/api/v1/tours/:id',(req,res)=>{
+    if(req.params.id * 1 >tours.length ){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'ID inválido'
+        })
+    }
+    res.status(200).json({
+        status:'success',
+        data: {
+            tour: 'Tour actualizado...'
+        }
+    })
 })
 
 
