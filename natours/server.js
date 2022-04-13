2d9b33cc1d93824a7af5ffa4318d546cfc7d6773
JavierGold <567498jav@gmail.com>
2022-04-13 18:36:56 -0500

    Modelo mongoose

--- natours/server.js
+++ natours/server.js
@@ -13,8 +13,27 @@ mongoose.connect(DB,{
     console.log('Conexión exitosa...')
 })
 
-const port = 3000;
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Se necesita un nombre'],
+        unique: true
+    },
+    rating: {
+        type:Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'Se necesita un precio']
+    }
+});
 
+const Tour = mongoose.model('Tour',tourSchema);
+
+
+const port = 3000;
 app.listen(port,()=>{
     console.log(`App corriendo en el puerto ${port}`);
 });
+
