5759d0a1abc44cca1f07f3d2267495dd9e868c91
JavierGold <567498jav@gmail.com>
2022-04-13 16:12:44 -0500

    param middleware

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,6 +2,28 @@ const fs = require('fs')
 
 const tours = JSON.parse( fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
+exports.checkID = (req,res,next,val)=>{
+    console.log(`El id del tour es ${val}`)
+    if(req.params.id * 1 >tours.length ){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'ID inválido'
+        })
+    }
+    next();
+}
+
+
+exports.checkBody = (req,res,next)=>{
+    if(!req.body.name || !req.body.price){
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price...'
+        })
+    }
+    next();
+}
+
 exports.getAllTours = (req,res)=>{
     console.log(req.requestTime)
     res.status(200).json({
@@ -16,12 +38,6 @@ exports.getAllTours = (req,res)=>{
 
 exports.getTour = (req,res)=>{
     const id=req.params.id * 1 //al multiplicar hace de un string un int 
-    if(id >tours.length || !tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'ID inválido'
-        })
-    }
     const tour = tours.find(el=>el.id===id)
     res.status(200).json({
         status: 'success',
@@ -47,12 +63,7 @@ exports.createTour=(req,res)=>{
  }
 
  exports.updateTour = (req,res)=>{
-    if(req.params.id * 1 >tours.length ){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'ID inválido'
-        })
-    }
+    
     res.status(200).json({
         status:'success',
         data: {
@@ -63,12 +74,7 @@ exports.createTour=(req,res)=>{
 
 
 exports.deleteTour =(req,res)=>{
-    if(req.params.id * 1 >tours.length ){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'ID inválido'
-        })
-    }
+   
     res.status(204).json({
         status:'success',
         data: null
