a90cbc1637a1ca7d20593f3b42cc616274b2468e
JavierGold <567498jav@gmail.com>
2022-05-05 12:53:29 -0500

    fix: Tour model part 1

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -3,15 +3,39 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Se necesita un nombre'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration:{
+        type:Number,
+        required: [true, 'Se necesita una duración'],
+    },
+    maxGroupSize:{
+        type:Number,
+        required: [true, 'Se necesita un tamaño máximo'],
+    },
+    difficulty:{
+        type: String,
+        required: [true, 'Se necesita una dificultad'],
+    },
+    ratingsAverage: {
         type:Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type:Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'Se necesita un precio']
+    },
+    priceDiscount: {
+        type: Number,
+    },
+    summary:{
+        type: String,
+        trim: true
     }
 });
 
