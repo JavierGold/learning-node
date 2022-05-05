ae5496f24e95a364c686f53fdb8196db72d8356b
JavierGold <567498jav@gmail.com>
2022-05-05 11:11:51 -0500

    add: update tourController

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -27,7 +27,7 @@ exports.checkID = (req,res,next,val)=>{
 
 exports.getAllTours = async (req, res) => {
 
-    
+
 
     try {
         const tours = await Tour.find();
@@ -51,15 +51,15 @@ exports.getTour = async (req, res) => {
 
 
     try {
-       const tour = await Tour.findById(req.params.id);
+        const tour = await Tour.findById(req.params.id);
 
-       res.status(200).json({
-        status: 'success',
-        data:{
-            tour
-        }
-    })
-      
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+
     } catch (err) {
         res.status(400).json({
             status: 'fail',
@@ -99,16 +99,29 @@ exports.createTour = async (req, res) => {
 
 }
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+    try {
+      const tour =  await Tour.findByIdAndUpdate(req.params.id, req.body,{
+            new: true,
+            runValidators: true
+        })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: tour
+            }
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 
 
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Tour actualizado...'
-        }
-    })
 }
 
 
