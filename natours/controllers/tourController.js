0bf1e2f0669ae9b869c9e6c6ee6abab774616479
JavierGold <567498jav@gmail.com>
2022-05-06 12:39:06 -0500

    add: matching and grouping

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -3,7 +3,7 @@ const Tour = require('../models/tourModel')
 
 const APIFeatures = require('./../utils/apiFeatures')
 
-exports.aliasTopTours = (req,res,next) => {
+exports.aliasTopTours = (req, res, next) => {
     req.query.limit = '5'
     req.query.sort = 'price'
     next()
@@ -121,5 +121,56 @@ exports.deleteTour = async (req, res) => {
         })
     }
 
+
+}
+
+exports.getTourStats = async (req, res) => {
+
+    try {
+
+        const stats = await Tour.aggregate([{
+                $match: {
+                    ratingsAverage: {
+                        $gte: 4.5
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id:'$difficulty',
+                    numTours:{
+                        $sum: 1
+                    },
+                    numRatings:{
+                        $sum: '$ratingsQuantity'
+                    },
+                    avgRating: {
+                        $avg: '$ratingsAverage'
+                    },
+                    avgPrice: {
+                        $avg: '$price'
+                    },
+                    minPrice: {
+                        $min: 'price'
+                    },
+                    maxPrice: {
+                        $max: 'price'
+                    }
+                }
+            }
+        ])
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats: stats
+            }
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 
 }
