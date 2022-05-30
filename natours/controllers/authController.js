0b1b9ab56744d735a696625ef8d090852428acf1
JavierGold <567498jav@gmail.com>
2022-05-30 15:35:33 -0500

    add: role in user model

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -2,25 +2,29 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
-const {promisify}=require('util');
+const {
+    promisify
+} = require('util');
 
-const signToken = id =>{
-    return jwt.sign({id: id}, process.env.JWT_SECRET,{
+const signToken = id => {
+    return jwt.sign({
+        id: id
+    }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 }
 
-exports.signup = catchAsync( async (req,res,next)=>{
+exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
-      });
+    });
 
 
-      const token =signToken(newUser._id)
-      
+    const token = signToken(newUser._id)
+
 
     res.status(201).json({
         status: 'success',
@@ -32,52 +36,69 @@ exports.signup = catchAsync( async (req,res,next)=>{
 })
 
 
-exports.login =catchAsync (async (req,res,next)=>{
-    const {email,password} = req.body;
+exports.login = catchAsync(async (req, res, next) => {
+    const {
+        email,
+        password
+    } = req.body;
 
     //checar si existen
 
-    if(!email || !password){
-      return  next(new AppError('Please provide email and password', 400))
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password', 400))
     }
     // checar si el usuario existe y la contraseña es correcta
 
-    const user = await User.findOne({email}).select('+password')
+    const user = await User.findOne({
+        email
+    }).select('+password')
 
-    if(!user || !(await user.correctPassword(password, user.password))){
-        return  next(new AppError('Incorrect email or password', 401))
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401))
     }
 
     //si todo está bien envía el token al user
-    const token=signToken(user._id)
+    const token = signToken(user._id)
     res.status(200).json({
-        status:'success',
+        status: 'success',
         token
     })
 })
 
-exports.protect = catchAsync(async(req,res,next)=>{
+exports.protect = catchAsync(async (req, res, next) => {
 
     // obtener el token y checar si existe
 
     let token
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-         token = req.headers.authorization.split(' ')[1]
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1]
     }
 
-    if(!token){
-        return next(new AppError('You are not logged in! Please log in to get access.',401))
+    if (!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401))
     }
 
     //validar el token
 
-    jwt.verify(token, process.env.JWT_SECRET,)
-
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET, )
 
     //checar si el usuario existe
+    const currentUser = await User.findById(decoded.id)
 
-    //checar si el usuario cambio de contraseña después que se tuvo el token
+    if (!currentUser) {
+        return next(new AppError('The user that has this token does no longer exists', 401))
+    }
 
+    //checar si el usuario cambio de contraseña después que se hizo el token
+    
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError('User recently changed password! Please log in again.', 401)
+        );
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
     next();
 });
