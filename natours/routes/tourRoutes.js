e770566b204efb09d3aefb1398bfaf81ec3f263f
JavierGold <567498jav@gmail.com>
2022-06-03 12:39:52 -0500

    fix: tourRoutes 3

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -33,7 +33,7 @@ router
     .route('/:tourId/reviews')
     .post(
         authController.protect,
-        authController.restrictTo('users'),
+        authController.restrictTo('user'),
         reviewController.createReview
     );    
 
