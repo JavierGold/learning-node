4f01c65dbe2ff061d3810fac587572b08883d826
JavierGold <567498jav@gmail.com>
2022-06-04 15:16:59 -0500

    fix: review model

--- natours/models/reviewModel.js
+++ natours/models/reviewModel.js
@@ -76,6 +76,13 @@ const reviewSchema = new mongoose.Schema(
     this.constructor.calcAverageRatings(this.tour);
   });
 
+
+  reviewSchema.pre(/^findOneAnd/, async function(next) {
+    this.r = await this.findOne();
+    // console.log(this.r);
+    next();
+  });
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
