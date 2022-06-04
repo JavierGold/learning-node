84a42c6abe853d03fe16cc757a85abcfe6afa0cb
JavierGold <567498jav@gmail.com>
2022-06-03 21:31:26 -0500

    add: handlerfactory CREATE reviews

--- natours/controllers/reviewController.js
+++ natours/controllers/reviewController.js
@@ -2,8 +2,17 @@ const Review = require('./../models/reviewModel');
 const catchAsync= require('./../utils/catchAsync')
 const factory = require('./handlerFactory');
 
+exports.setTourUserIds = (req, res, next) => {
+    
+    if(!req.body.tour)req.body.tour = req.params.tourId;
+    req.body.user = req.user.id;
+
+    next();
+  };
+
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
+exports.createReview = factory.createOne(Review);
 
 exports.getAllReviews = catchAsync( async (req,res,next)=>{
     
@@ -22,7 +31,7 @@ exports.getAllReviews = catchAsync( async (req,res,next)=>{
 });
 
 
-exports.createReview = catchAsync( async (req,res,next)=>{
+/* exports.createReview = catchAsync( async (req,res,next)=>{
 
     if(!req.body.tour)req.body.tour = req.params.tourId;
     req.body.user = req.user.id;
@@ -35,4 +44,4 @@ exports.createReview = catchAsync( async (req,res,next)=>{
             newReview
         }
       });
-});
+}); */
