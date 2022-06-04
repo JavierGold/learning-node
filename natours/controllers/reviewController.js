7d77da7f8d1783fbb5d677a82c0ce1e62990db55
JavierGold <567498jav@gmail.com>
2022-06-03 19:57:48 -0500

    add: handlerfactory delete

--- natours/controllers/reviewController.js
+++ natours/controllers/reviewController.js
@@ -1,5 +1,9 @@
 const Review = require('./../models/reviewModel');
 const catchAsync= require('./../utils/catchAsync')
+const factory = require('./handlerFactory');
+
+
+exports.deleteReview = factory.deleteOne(Review);
 
 exports.getAllReviews = catchAsync( async (req,res,next)=>{
     
