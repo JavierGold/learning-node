1ed454672016e28fda6c33020f8a62ac1312d9d9
JavierGold <567498jav@gmail.com>
2022-06-08 11:30:41 -0500

    add: account page

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -41,3 +41,9 @@ exports.getOverview = catchAsync(async (req, res, next) => {
       title: 'Log into your account'
     });
   };
+
+  exports.getAccount = (req, res) => {
+    res.status(200).render('account', {
+      title: 'Your account'
+    });
+  };
--- natours/routes/viewRoutes.js
+++ natours/routes/viewRoutes.js
@@ -9,5 +9,6 @@ router.get('/', authController.isLoggedIn, viewsController.getOverview)
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
