f2a47afa376bfbf8ce0a865a21c0df11e6db23fa
JavierGold <567498jav@gmail.com>
2022-06-14 17:16:18 -0500

    add: updateMe photo

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -2,6 +2,35 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+const multer = require('multer');
+
+
+ const multerStorage = multer.diskStorage({
+   destination: (req, file, cb) => {
+     cb(null, 'public/img/users');
+   },
+  filename: (req, file, cb) => {
+     const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+   }
+ });
+
+ const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+/* const upload = multer({dest: 'public/img/users'}); */
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -38,7 +67,7 @@ exports.updateMe= catchAsync( async(req,res,next)=>{
   //poner los campos que se pueden cambiar
 
   const filteredBody = filterObj(req.body, 'name', 'email');
-
+  if (req.file) filteredBody.photo = req.file.filename;
   //actualizar el user document
 
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const userController = require('./../controllers/userController')
 const authController = require('./../controllers/authController')
 
+
+
 const router = express.Router();
 
 router.post('/signup',authController.signup);
@@ -16,7 +18,7 @@ router.use(authController.protect);
 router.patch('/updatePassword' ,authController.updatePassword);
 
 router.get('/me',  userController.getMe, userController.getUser);
-router.patch('/updateMe',userController.updateMe);
+router.patch('/updateMe',userController.uploadUserPhoto,userController.updateMe);
 router.delete('/deleteMe',userController.deleteMe);
 
 
